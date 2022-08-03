7ccc285ae7d98f1569eab57b7f8d59afd6aae47b
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-08-03 16:12:41 -0500

    feat: upgrade ♻️ login user case

--- src/application/use-cases/user-login-usecase.js
+++ src/application/use-cases/user-login-usecase.js
@@ -1,22 +1,31 @@
 import { InvalidLoginExecption } from "../errors/application.login.execption.js";
-import bcrypt from "bcrypt";
+import { EmailVO } from "../../domain/value-object/email.vo.js";
+import { VOFormatException } from "../../domain/errors/vo.format.exception.js";
+import { PlainPasswordVO } from "../../domain/value-object/plain.password.vo.js";
 
 export class UserLoginUseCase {
-    constructor({ userRepository }){
-        this.userRepository = userRepository;
-    }
+	constructor({ userRepository }) {
+		this.userRepository = userRepository;
+	}
 
-    async execute(email, password) {
-        const existingUser = await this.userRepository.findByEmail(email);
-        if (!existingUser) {
-            throw new InvalidLoginExecption();
-        }
+	async execute(email, password) {
+		try {
+			const userEmail = new EmailVO(email);
+			const userPassword = new PlainPasswordVO(password);
+			const existingUser = await this.userRepository.findByEmail(userEmail);
+			if (!existingUser) {
+				throw new InvalidLoginExecption();
+			}
 
-        const passwordMatch = await bcrypt.compare(password, existingUser.password);
-        if (!passwordMatch) {
-            throw new InvalidLoginExecption();
-        }
-        return existingUser.id;
-    }
+			const passwordMatch = await existingUser.password.compare(userPassword);
+			if (!passwordMatch) {
+				throw new InvalidLoginExecption();
+			}
+			return existingUser.id;
+		} catch (error) {
+			if (error instanceof VOFormatException) throw new InvalidLoginExecption();
 
-};
+			throw error;
+		}
+	}
+}
