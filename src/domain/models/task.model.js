ee6b1c3b726c2d429dfb9707263102754da7ba34
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-08-08 10:14:22 -0500

    feat: model task

---
+++ src/domain/models/task.model.js
@@ -0,0 +1,31 @@
+import { InvalidTaskFormatException } from "../errors/invalid.task.format.exception.js";
+import { TitleTaskVO } from "../value-object/task-value-object/title.vo.js";
+import { DescripTaskVO } from "../value-object/task-value-object/description.vo.js";
+import { StatusTaskVO } from "../value-object/task-value-object/status.vo.js";
+import { UuidVO } from "../value-object/uuid.vo";
+
+export class TaskModel {
+	/**
+	 * @param {UuidVO} id
+	 * @param {titleVO} title
+	 * @param {descriptionVO} description
+	 * @param {statusVO} status
+	 */
+	constructor(id, title, description, status) {
+		this.assertIsValid(id, title, description, status);
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.status = status;
+	}
+
+	assertIsValid(id, title, description, status) {
+		if (
+			!(id instanceof UuidVO) ||
+			!(title instanceof TitleTaskVO) ||
+			!(description instanceof DescripTaskVO) ||
+			!(status instanceof StatusTaskVO)
+		)
+			throw new InvalidTaskFormatException();
+	}
+}
