4196190d8af177d532116380285f5a48fbedc3ab
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-08-09 11:02:30 -0500

    feat: error middleware 🛑

--- src/infrastruture/middlewares/error.middleware.js
+++ src/infrastruture/middlewares/error.middleware.js
@@ -1,4 +1,4 @@
-import { ApplicationConflictExecption } from "../../application/errors/application.conflic.execption.js";
+import { ApplicationConflictExecption } from "../../application/errors/application.conflict.execption.js";
 import { ApplicationUnauthorizedExecption } from "../../application/errors/application.unauthorized.execption.js";
 import { DomainFormatException } from "../../domain/errors/domian.format.exception.js";
 import { InfrastrutureFormatException } from "../errors/infrastruture.format.exception.js";
@@ -10,12 +10,11 @@ export const errorMiddleware = (error, _, res, next) => {
 	)
 		return res.status(400).send(error.message);
 
-	if (error instanceof ApplicationConflictExecption){
+	if (error instanceof ApplicationConflictExecption) {
 		return res.status(409).send(error.message);
 	}
-	if(error instanceof ApplicationUnauthorizedExecption)
-	   return res.status(403).send(error.message);
-	
+	if (error instanceof ApplicationUnauthorizedExecption)
+		return res.status(403).send(error.message);
 
 	return res.status(500).send("Error into server");
 };
