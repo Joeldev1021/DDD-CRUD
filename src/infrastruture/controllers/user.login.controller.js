cd79c5910556f7b344ac52abe2b6a2aad52e3fc9
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-08-09 11:03:20 -0500

    feat: user controller login 🚧

--- src/infrastruture/controllers/user.login.controller.js
+++ src/infrastruture/controllers/user.login.controller.js
@@ -3,25 +3,24 @@ import { UnnecessaryFieldsException } from "../errors/unnecesary.fields.exceptio
 import { signJwtAsync } from "../service/jwt.service.js";
 
 export class UserLoginController {
-    constructor({userLoginUseCase}){
-        this.userLoginUseCase = userLoginUseCase;
-    }
+	constructor({ userLoginUseCase }) {
+		this.userLoginUseCase = userLoginUseCase;
+	}
 
-    async execute(req, res, next) {
-        const { email, password, ...rest } = req.body;
-        try {
-            if (!email || !password) throw new MissingFieldsFormatException();
+	async execute(req, res, next) {
+		const { email, password, ...rest } = req.body;
+		try {
+			if (!email || !password) throw new MissingFieldsFormatException();
 
-            if (Object.keys(rest).length > 0) throw new UnnecessaryFieldsException();
+			if (Object.keys(rest).length > 0) throw new UnnecessaryFieldsException();
 
-            const id = await await this.userLoginUseCase.execute(email, password);
-            const payload = { id };
-            const signOptions = { algorithm: "HS512", expiresIn: "2d" };
-            const token = await signJwtAsync(payload, signOptions);
-            return res.send({ token });
-        } catch (error) {
-            next(error);
-        }
-    }
-
-};
+			const id = await await this.userLoginUseCase.execute(email, password);
+			const payload = { id };
+			const signOptions = { algorithm: "HS512", expiresIn: "2d" };
+			const token = await signJwtAsync(payload, signOptions);
+			return res.send({ token });
+		} catch (error) {
+			next(error);
+		}
+	}
+}
